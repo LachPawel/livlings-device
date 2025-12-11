4a33fef772d80c701d7da395b82e335443884591
KowalewskiPawel <pawelkowalewsk@gmail.com>
2025-12-11 18:31:23 +0100

    Initial Commit

---
+++ livlings device/livlings_deviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  livlings_deviceApp.swift
+//  livlings device
+//
+//  Created by Pawel Kowalewski on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct livlings_deviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
